b9ed2a926a8b95effaa9409d48de675a3d637fab
addek-lab <ofxffm@gmail.com>
2026-07-31 04:25:30 +0200

    fix: Remove setsid shell command - daemon now handles fork+setsid internally in C

--- launch.sh
+++ launch.sh
@@ -37,5 +37,5 @@ EXIT_CODE=$?
 
 # If user selected Minimize to Background (exit code 42)
 if [ $EXIT_CODE -eq 42 ]; then
-	setsid "$DIR/bin/$PLATFORM/musicplayer.elf" --daemon > /dev/null 2>&1 &
+	"$DIR/bin/$PLATFORM/musicplayer.elf" --daemon > /dev/null 2>&1 &
 fi
